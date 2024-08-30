184e7a5e9b13d47d2ffaa9fb91cb291707650b87
Jashwanth Kurma <jashukurma@gmail.com>
2024-08-30 21:29:20 +0530

    added authlayout template and card component

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { set } from "react-hook-form";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function AuthLayout({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    setLoading(true);
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoading(false);
+  }, [authStatus, authentication, navigate]);
+
+  return loading ? <h1>LOADING</h1> : { children };
+}
+
+export default AuthLayout;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,13 @@ import service from "../appwrite backend/service";
 import { Link } from "react-router-dom";
 
 function Card({ $id, title, image }) {
+  const imageSrc = service.getFilePreview(image);
+
   return (
-    <Link to={`post/${$id}`}>
+    <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4 ">
         <div className="w-full justify-center mb-4">
-          <img
-            src={service.getFilePreview(image)}
-            alt={title}
-            className="rounded-xl"
-          />
+          <img src={imageSrc} alt={title} className="rounded-xl" />
         </div>
         <h2 className="text-xl font-bold">{title}</h2>
       </div>
